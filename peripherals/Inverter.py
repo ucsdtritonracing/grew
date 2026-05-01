e0f2c598d2e667c4955afa518126cf0d276f6d05
transparentglass2507 <lec017@ucsd.edu>
2026-05-01 01:27:51 -0700

    implement CAN data -> UI framework

--- peripherals/Inverter.py
+++ peripherals/Inverter.py
@@ -1,25 +1,23 @@
 from constants import InverterConstants
 from peripherals.CANPeripheral import CANPeripheral
 import cantools
-from PySide6.QtCore import Slot
+from PySide6.QtCore import Slot, Signal
     
 class Inverter(CANPeripheral):
     const = InverterConstants()
     func = lambda self, msg: self.on_message_received(msg)
+    dataSignal = Signal(list,str)
     def __init__(self, bus):
         super().__init__(id=self.const.DEVICE_ID, isExtended=False, bus=bus, func=self.func)
     def setup(self):
         self.state = {
-            "motorInfo": [0,0,0], #motor speed, motor mechanical angle, motor temp
-            "temps": [0,0,0,0], # A/B/C/control board/
-            "commandedTorque": 0,
-            "torqueFeedback": 0,    
-            "inverterEnabled": False,
-            "inverterLockout": False,
-            "forward" : False,
+            "motorInfo":    [0,0,0], #motor speed, motor mechanical angle, motor temp
+            "tempInfo":     [0,0,0,0], # A/B/C/control board/
+            "torqueInfo":   [0,0,0], # commanded torque, torqueFeedback, torque capability
+            "inverterInfo": [False,False,False] # inverter enabled, lockout, forward
         }
         self.db = cantools.database.load_file("constants\20240815_PM_and_RM_CAN_DB.dbc")
-    
+        
     @Slot()
     def enable(self):
         #send vcu disable device message here?
@@ -35,24 +33,30 @@ class Inverter(CANPeripheral):
             case self.const.MOTOR_INFO_ID:
                 self.state["motorInfo"][0] = [data["INV_Motor_Speed"]]
                 self.state["motorInfo"][1] = [data["INV_Motor_Angle_Electrical"] / self.const.POLE_PAIRS]
+                self.dataSignal.emit(self.state["motorInfo"],"motor")
             case self.const.TEMPS_ID_1:
-                self.state["temps"][0] = [data["INV_Module_A"]]
-                self.state["temps"][1] = [data["INV_Module_B"]]
-                self.state["temps"][2] = [data["INV_Module_C"]]
+                self.state["tempInfo"][0] = [data["INV_Module_A"]]
+                self.state["tempInfo"][1] = [data["INV_Module_B"]]
+                self.state["tempInfo"][2] = [data["INV_Module_C"]]
+                self.dataSignal.emit(self.state["tempInfo"])
             case self.const.TEMPS_ID_2:
-                self.state["temps"][3] = [data["INV_Control_Board_Temperature"]]
+                self.state["tempInfo"][3] = [data["INV_Control_Board_Temperature"]]
+                self.dataSignal.emit(self.state["tempInfo"],"temp")
             case self.const.TEMPS_ID_3:
                 self.state["motorInfo"][2] = [data["INV_Motor_Temperature"]]
+                self.dataSignal.emit(self.state["motorInfo"],"motor")
             case self.const.STATES_ID:
-                self.state["forward"] = data["INV_Direction_Command"] == 1
-                self.state["inverterEnabled"] = data["INV_Inverter_Enable_State"] == 1
+                self.state["inverterInfo"] = data["INV_Direction_Command"] == 1
+                self.state["inverterInfo"] = data["INV_Inverter_Enable_State"] == 1
+                self.dataSignal.emit(self.state["inverterInfo"],"inverter")
             case self.const.TORQUES_ID:
-                self.state["commandedTorque"] = data["INV_Commanded_Torque"]
-                self.state["torqueFeedback"] = data["INV_Torque_Feedback"]
+                self.state["torqueInfo"][0] = data["INV_Commanded_Torque"]
+                self.state["torqueInfo"][1] = data["INV_Torque_Feedback"]
+                self.dataSignal.emit(self.state["torqueInfo"],"torque")
+                
     
     def on_message_received(self, msg):
         self.processMessage(msg)
-        self.updateUI()
 
 
     @Slot()
--- widgets/InverterWidget.py
+++ widgets/InverterWidget.py
@@ -1,13 +1,27 @@
 from PySide6.QtUiTools import QUiLoader
 from functools import partial
+from widgets import PeripheralWidget
 from peripherals import Inverter 
+from PySide6.QtCore import Slot, Signal, QFile
 
 class InverterWidget(PeripheralWidget):
     def __init__(self, bus):
         super().__init__(Inverter(bus))
+        inverter = super().getPeripheral()
         ui_file = QFile("barebonesMain.ui")
         ui_file.open(QFile.ReadOnly)
+
+        inverter.dataSignal.connect(self.updateUI)
 
         loader = QUiLoader()
         window = loader.load(ui_file)
         window.show()
+    
+    @Slot(list)
+    def updateUI(self, data, name):
+        #update ui data here
+        i = 0
+    
+
+    
+
