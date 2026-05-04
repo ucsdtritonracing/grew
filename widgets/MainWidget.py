9771a78d29ec5ca7df98a5a163407fa3f32b3bd7
transparentglass2507 <lec017@ucsd.edu>
2026-05-04 12:35:46 -0700

    add support for 2 peripherals, rename inverter widget

--- widgets/MainWidget.py
+++ widgets/MainWidget.py
@@ -1,18 +1,19 @@
 from PySide6.QtUiTools import QUiLoader
 from functools import partial
 from widgets import PeripheralWidget
-from peripherals import Inverter 
+from peripherals import Inverter, VCU
 from PySide6.QtCore import Slot, Signal, QFile
 
-class InverterWidget(PeripheralWidget):
+class MainWidget(PeripheralWidget):
     def __init__(self, bus):
-        super().__init__(Inverter(bus))
+        super().__init__(VCU(bus),Inverter(bus))
         inverter = super().getPeripheral()
+        vcu = super().getPeripheral1()
         ui_file = QFile("barebonesMain.ui")
         ui_file.open(QFile.ReadOnly)
 
         inverter.dataSignal.connect(self.updateUI)
-
+        vcu.dataSignal.connect(self.updateUI)
         loader = QUiLoader()
         window = loader.load(ui_file)
         window.show()
