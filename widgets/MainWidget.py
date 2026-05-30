5eb46b4511ba84f1130261ff1a62edf7954137e8
Your Nameamrita.sivanesan2025@gmail.com <amrita.sivanesan2025@gmail.com>
2026-05-29 17:13:41 -0700

    dbc file changed

--- widgets/MainWidget.py
+++ widgets/MainWidget.py
@@ -16,7 +16,7 @@ class MainWidget(QtWidgets.QMainWindow):
         self.vcuWidget = VCUWidget(peripheral2)
 
         super().__init__()
-        ui_file = QFile("ui\\carTestingWebappMain.ui")
+        ui_file = QFile("ui/carTestingWebappMain.ui")
         ui_file.open(QFile.ReadOnly)
         loader = QUiLoader()
         loader.registerCustomWidget(pg.PlotWidget)
